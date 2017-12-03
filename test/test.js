659e1faa2e32f0c0ace03d7c14f328e68cad8527
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-12-03 19:47:20 +0100

    test basic scenarios

--- test/test.js
+++ test/test.js
@@ -8,6 +8,9 @@ describe.only('version control', () => {
   beforeEach(async () => {
     jsreport = JsReport()
     jsreport.use(require('jsreport-templates')())
+    jsreport.use(require('jsreport-data')())
+    jsreport.use(require('jsreport-phantom-pdf')())
+    jsreport.use(require('jsreport-assets')())
     jsreport.use(require('../')())
     await jsreport.init()
     collection = jsreport.documentStore.collection('templates')
@@ -30,6 +33,24 @@ describe.only('version control', () => {
     templates.should.have.length(0)
   })
 
+  it('revert should recover localy removed file', async () => {
+    await collection.insert({name: 'foo'})
+    await jsreport.versionControl.commit('1')
+    collection.remove({})
+    await jsreport.versionControl.revert()
+    const templates = await collection.find({})
+    templates.should.have.length(1)
+  })
+
+  it('revert should remove new local file', async () => {
+    await collection.insert({name: 'foo'})
+    await jsreport.versionControl.commit('1')
+    await collection.insert({name: 'foo2'})
+    await jsreport.versionControl.revert()
+    const templates = await collection.find({})
+    templates.should.have.length(1)
+  })
+
   it('history should list changed files', async () => {
     await collection.insert({name: 'foo'})
     await jsreport.versionControl.commit('1')
@@ -37,19 +58,23 @@ describe.only('version control', () => {
     await jsreport.versionControl.commit('2')
     const history = await jsreport.versionControl.history()
     history.should.have.length(2)
-    history[0].message.should.be.eql('1')
-    history[1].message.should.be.eql('2')
+    history[0].message.should.be.eql('2')
+    history[1].message.should.be.eql('1')
   })
 
   it('checkout should change local state to the particular commit', async () => {
     await collection.insert({name: 'foo', content: '1'})
-    const commit = await jsreport.versionControl.commit('1')
+    await jsreport.versionControl.commit('1')
     await collection.update({name: 'foo'}, {$set: { content: '2' }})
     await jsreport.versionControl.commit('2')
-    await jsreport.versionControl.checkout(commit._id)
+    await collection.update({name: 'foo'}, {$set: { content: '3' }})
+    const commit3 = await jsreport.versionControl.commit('3')
+    await collection.update({name: 'foo'}, {$set: { content: '4' }})
+    await jsreport.versionControl.commit('4')
+    await jsreport.versionControl.checkout(commit3._id)
     const templates = await collection.find({})
     templates.should.have.length(1)
-    templates[0].content.should.be.eql('1')
+    templates[0].content.should.be.eql('3')
   })
 
   it('diff should list files changes', async () => {
@@ -60,11 +85,52 @@ describe.only('version control', () => {
     diff[0].path.should.be.eql('foo')
   })
 
-  /*it.only('commit should store diffs for document props separately', async () => {
-    await collection.insert({name: 'foo', content: '1'})
+  it('commit should store diffs for document properties separately', async () => {
+    await collection.insert({name: 'foo', content: '1', helpers: '1'})
     await jsreport.versionControl.commit('1')
-    await collection.update({name: 'foo'}, {$set: { content: '1\n2' }})
+    await collection.update({name: 'foo'}, {$set: { content: '2', helpers: '2' }})
     const commit = await jsreport.versionControl.commit('2')
-    console.log(commit)
-  }) */
+    commit.changes.should.have.length(1)
+    const patch = JSON.parse(commit.changes[0].serializedPatch)
+    patch.documentProperties.should.have.length(2)
+  })
+
+  it('commit should store diffs only for changed document props', async () => {
+    await collection.insert({name: 'foo', content: '1', helpers: '1'})
+    await jsreport.versionControl.commit('1')
+    await collection.update({name: 'foo'}, {$set: { content: '2' }})
+    const commit = await jsreport.versionControl.commit('2')
+    commit.changes.should.have.length(1)
+    const patch = JSON.parse(commit.changes[0].serializedPatch)
+    patch.documentProperties.should.have.length(1)
+  })
+
+  it('should work for multiple entity types', async () => {
+    await collection.insert({name: 'foo'})
+    await jsreport.documentStore.collection('data').insert({ name: 'foo', shortid: 'a' })
+    await jsreport.versionControl.commit('1')
+    await jsreport.documentStore.collection('data').update({ name: 'foo' }, { $set: { shortid: 'b' } })
+    await jsreport.versionControl.revert()
+    const dataItems = await jsreport.documentStore.collection('data').find({})
+    dataItems.should.have.length(1)
+    dataItems[0].shortid.should.be.eql('a')
+  })
+
+  it('should correctly recover dates', async () => {
+    await jsreport.documentStore.collection('data').insert({ name: 'foo' })
+    await jsreport.versionControl.commit('1')
+    await jsreport.versionControl.revert()
+    const data = await jsreport.documentStore.collection('data').find({})
+    data[0].creationDate.should.be.Date()
+  })
+
+  it('commit should store diffs for nested document properties', async () => {
+    await collection.insert({ name: 'foo', phantom: { header: 'header' } })
+    await jsreport.versionControl.commit('1')
+    await collection.update({ name: 'foo' }, { $set: { phantom: { header: 'header2' } } })
+    const commit = await jsreport.versionControl.commit('2')
+    const patch = JSON.parse(commit.changes[0].serializedPatch)
+    patch.documentProperties.should.have.length(1)
+    patch.documentProperties[0].path.should.be.eql('phantom.header')
+  })
 })
