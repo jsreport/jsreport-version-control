13fc2287ce6dc2bfdc76534960141a86a16141fe
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-12-05 13:58:21 +0100

    display scrollbar

--- studio/LocalChangesEditor.js
+++ studio/LocalChangesEditor.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Studio from 'jsreport-studio'
 import ChangesTable from './ChangesTable.js'
+import style from './VersionControl.scss'
 
 export default class LocalChangesEditor extends Component {
   constructor (props) {
@@ -52,21 +53,19 @@ export default class LocalChangesEditor extends Component {
   render () {
     return (
       <div className='block custom-editor'>
+        <h1><i className='fa fa-history' /> uncommited changes</h1>
+        <div className='form-group'>
+          <label>Message</label>
+          <input type='text' value={this.state.message} onChange={(event) => this.setState({message: event.target.value, error: null})} />
+          <span style={{color: 'red', display: this.state.error ? 'block' : 'none'}}>{this.state.error}</span>
+        </div>
         <div>
-          <h1><i className='fa fa-history' /> uncommited changes</h1>
-          <div className='form-group'>
-            <label>Message</label>
-            <input type='text' value={this.state.message} onChange={(event) => this.setState({message: event.target.value, error: null})} />
-            <span style={{color: 'red', display: this.state.error ? 'block' : 'none'}}>{this.state.error}</span>
-          </div>
-          <div>
-            <button className='button confirmation' onClick={() => this.commit()}>Commit</button>
-            <button className='button danger' onClick={() => this.revert()}>Revert</button>
-            <button className='button confirmation' onClick={() => this.load()}>Refresh</button>
-          </div>
-          <div className='block-item' style={{marginTop: '2rem'}}>
-            {this.state.diff ? <ChangesTable changes={this.state.diff} /> : ''}
-          </div>
+          <button className='button confirmation' onClick={() => this.commit()}>Commit</button>
+          <button className='button danger' onClick={() => this.revert()}>Revert</button>
+          <button className='button confirmation' onClick={() => this.load()}>Refresh</button>
+        </div>
+        <div className={style.listContainer + ' block-item'}>
+          {this.state.diff ? <ChangesTable changes={this.state.diff} /> : ''}
         </div>
       </div>
     )
